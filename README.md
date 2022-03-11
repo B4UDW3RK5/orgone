cbf302fabc97edbaac833ba0f439a3f633c5bfcc
cat K <cat@ff3366.com>
2022-03-11 14:14:35 +1030

    v. 0.1

--- README.md
+++ README.md
@@ -6,4 +6,23 @@
 └─────────┴──┘      ├──────   ├─────────┴──┘   └──┴─────────┘
                     └─────────┘
 ·· A       CLI       ORGONE       ENERGY       ACCUMULATOR ··
+
+howTo:
+
+   1. git clone https://github.com/B4UDW3RK5/orgone
+
+   2. cd orgone
+
+   3. go build orgone.go
+
+   4. ./orgone
+
+
+toDo:
+
+   1. user specified orgone quantity
+
+   2. colors
+
+   3. fluffier random seed
 ```
--- orgone.go
+++ orgone.go
@@ -6,26 +6,46 @@
 //                      └─────────┘
 //  ·· A       CLI       ORGONE       ENERGY       ACCUMULATOR ··
 
+//  toDo: user specified orgone quantity
+//        colors
+//        fluffier random seed
+
 package main
+
 import (
    "fmt"
    "math/rand"
    "time"
 )
-func main() {
-   source := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// generates a row of orgone
+func orgone() string {
+   // a seed for rand
+   var source = rand.New(rand.NewSource(time.Now().UnixNano()))
+   // a set of shapes
    shapes := [13]string{"█","▌","▐","▖","▗","▘","▙","▚","▛","▜","▝","▞","▟"}
-   columns := 9
-   rows := 4
+   // a number of columns
+   var columns int = 9
+   // clear any leftover energy
+   var energy string = ""
+   // accumulate new energy
+   for columns >= 1 {
+      shape := source.Intn(12 - 0)
+      energy = energy + shapes[shape]
+      columns = columns - 1
+   }
+   return energy
+}
+
+// main func
+func main() {
+   var rows int = 4
+   fmt.Println("A quantity of orgone was accumulated.")
    fmt.Println("┌─────────┐")
    for rows >= 1 {
-      column := columns
       fmt.Print("│")
-      for column >= 1 {
-         shape := source.Intn(12 - 0)
-         fmt.Print(shapes[shape])
-         column = column - 1
-      }
+      var energy string = orgone()
+      fmt.Print(energy)
       fmt.Println("│")
       rows = rows - 1
    }
